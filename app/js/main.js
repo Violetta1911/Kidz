7fbba96cd96d512307fbe2d80167013300e10fce
Violetta1911 <sva198366@gmail.com>
2021-02-19 22:51:54 +0200

    added header/main slider/block/feature/more/courses/slider-teachrs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(function(){
+    $('.main-slider__inner').slick({
+        arrows: true,   
+        speed: 300,
+        centerMode: true,
+      variableWidth: true
+    });
 
+    $('.teachers__slider').slick({
+        arrows: true,   
+        speed: 300,
+        slidesToShow: 4,
+        slideToScroll: 1
+
+    });
    
+    
 });
