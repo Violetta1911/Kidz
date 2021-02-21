b01f6023ed1f57fa5b18a17ba62e1f412700c040
Violetta1911 <sva198366@gmail.com>
2021-02-21 21:32:44 +0200

    version for Andrew

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
     $('.main-slider__inner').slick({
+        infinite: true,
+        slidesToShow: 1,
         arrows: true,   
-        speed: 300,
-        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
       variableWidth: true
     });
 
@@ -12,6 +14,42 @@ $(function(){
         slidesToShow: 4,
         slideToScroll: 1
     });
+
+    window.onload = function() {
+        var drawingCanvas = document.getElementById('smile');
+        if(drawingCanvas && drawingCanvas.getContext) {
+         var context = drawingCanvas.getContext('2d');
+         // Рисуем окружность 
+         context.strokeStyle = "#000";
+         context.fillStyle = "#fc0";
+         context.beginPath();
+         context.arc(50,50,25,0,Math.PI*2,true);
+         context.closePath();
+         context.stroke();
+         context.fill();
+         // Рисуем левый глаз 
+         context.fillStyle = "#fff";
+         context.beginPath();
+         context.arc(42,45,4,0,Math.PI*2,true);
+         context.closePath();
+         context.stroke();
+         context.fill();
+         // Рисуем правый глаз 
+         context.beginPath();
+         context.arc(58,45,4,0,Math.PI*2,true);
+         context.closePath();
+         context.stroke();
+         context.fill();
+         // Рисуем рот
+         context.beginPath();
+         context.moveTo(30,58);
+         context.quadraticCurveTo(50,65,65,58);
+         context.quadraticCurveTo(50,75,35,58); 
+         context.closePath();
+         context.stroke();
+         context.fill();
+        }
+       }
    
     let mixer = mixitup ('.gallery__box'); 
     
