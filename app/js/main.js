7c8cd900c94fbfdb44d989ec5c55f4f7243a5720
Violetta1911 <sva198366@gmail.com>
2021-02-24 17:17:59 +0200

    addaptive finished

--- app/js/main.js
+++ app/js/main.js
@@ -4,52 +4,51 @@ $(function(){
         slidesToShow: 1,
         arrows: true,   
         autoplay: true,
-        autoplaySpeed: 2000,
-      variableWidth: true
+         autoplaySpeed: 2000,
+         variableWidth: true
     });
 
     $('.teachers__slider').slick({
         arrows: true,   
         speed: 300,
         slidesToShow: 4,
-        slideToScroll: 1
-    });
+        slideToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1300,
+              settings: {                
+                slidesToShow: 3,
+                centerMode: true,
+              }
+            },
+            {
+              breakpoint: 820,
+              settings: {
+                slidesToShow: 2,
+                centerMode: false,
+              }
+            },
+            {
+              breakpoint: 670,
+              settings: {
+               arrows:false,
+               slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 520,
+              settings: {
+               arrows:true,
+               slidesToShow: 1,
+              }
+            },
+            
+          ]
+    });    
 
-    window.onload = function() {
-        var drawingCanvas = document.getElementById('smile');
-        if(drawingCanvas && drawingCanvas.getContext) {
-         var context = drawingCanvas.getContext('2d');
-         // Рисуем окружность 
-         context.strokeStyle = "#000";
-         context.fillStyle = "#fc0";
-         context.beginPath();
-         context.arc(50,50,25,0,Math.PI*2,true);
-         context.closePath();
-         context.stroke();
-         context.fill();
-         // Рисуем левый глаз 
-         context.fillStyle = "#fff";
-         context.beginPath();
-         context.arc(42,45,4,0,Math.PI*2,true);
-         context.closePath();
-         context.stroke();
-         context.fill();
-         // Рисуем правый глаз 
-         context.beginPath();
-         context.arc(57,45,4,0,Math.PI*2,true);
-         context.closePath();
-         context.stroke();
-         context.fill();
-         // Рисуем рот
-         context.beginPath();
-         context.moveTo(30,58);
-         context.quadraticCurveTo(50,65,65,60);
-         context.quadraticCurveTo(50,75,35,60); 
-         context.closePath();
-         context.stroke();
-         context.fill();
-        }
-       }
+    $('.menu-btn').on('click', function (){
+      $('.navigation').slideToggle('')
+      });
    
     let mixer = mixitup ('.gallery__box'); 
     
